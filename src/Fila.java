09e0710ce9857b1ed6581be1b2741355c693d102
phillipcmittmann <54452466+phillipcmittmann@users.noreply.github.com>
2023-03-21 20:42:53 -0300

    Update Fila.java

--- src/Fila.java
+++ src/Fila.java
@@ -4,20 +4,22 @@ public class Fila {
 	private int capacidadeMaxFila;
 	private int estadoFila;
 	private double[] tempoEmCadaEstado;
-	int tempoMinChegada;
-	int tempoMaxChegada;
-	int tempoMinSaida;
-	int tempoMaxSaida;
+	private int tempoMinChegada;
+	private int tempoMaxChegada;
+	private int tempoMinSaida;
+	private int tempoMaxSaida;
+	private double tempoAnterior;
 	
 	public Fila(int qtdServidores, int capacidadeMaxFila, int tempoMinChegada, int tempoMaxChegada, int tempoMinSaida, int tempoMaxSaida) {
 		this.qtdServidores = qtdServidores;
 		this.capacidadeMaxFila = capacidadeMaxFila;
 		this.estadoFila = 0;
-		this.tempoEmCadaEstado = new double[capacidadeMaxFila];
+		this.tempoEmCadaEstado = new double[capacidadeMaxFila + 1];
 		this.tempoMinChegada = tempoMinChegada;
 		this.tempoMaxChegada = tempoMaxChegada;
 		this.tempoMinSaida = tempoMinSaida;
 		this.tempoMaxSaida = tempoMaxSaida;
+		this.tempoAnterior = 0;
 	}
 	
 	public void colocaNaFila() { this.estadoFila++; }
@@ -27,7 +29,10 @@ public class Fila {
 	
 	public int getCapacidadeMaxFila() { return this.capacidadeMaxFila; }
 	
-	public void setTempoEmCadaEstado(double tempo) { this.tempoEmCadaEstado[this.estadoFila] = tempo; }
+	public void setTempoEmCadaEstado(double tempo) { 
+		this.tempoEmCadaEstado[this.estadoFila] = tempo - this.tempoAnterior;
+		this.tempoAnterior = tempo;
+	 }
 	
 	public int getQtdServidores() { return this.qtdServidores; }
 	
@@ -35,6 +40,7 @@ public class Fila {
 	public int getTempoMaxChegada() { return this.tempoMaxChegada;  }
 	public int getTempoMinSaida() { return this.tempoMinSaida; }
 	public int getTempoMaxSaida() { return this.tempoMaxSaida; }
+	public double[] getTempoEstadosFila() { return this.tempoEmCadaEstado; }
 	
 	public String toStringTempoEstadosFila() { return this.tempoEmCadaEstado.toString(); }
 }
